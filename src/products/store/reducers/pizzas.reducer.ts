a61e562dab2c5f95577e82ead074cd357587c70b
littlephoenixstudios <63930643+stalwartphoenix-games@users.noreply.github.com>
2023-06-15 23:34:32 -0400

    Chapter 5 Checkpoint

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -2,13 +2,13 @@ import { Pizza } from "src/products/models/pizza.model";
 import * as fromPizzas from '../actions/pizzas.action';
 
 export interface PizzaState {
-    data: Pizza[];
+    entities: { [id: number]: Pizza};
     loaded: boolean;
     loading: boolean;
 }
 
 export const initialState: PizzaState = {
-    data: [],
+    entities: {},
     loaded: false,
     loading: false
 };
@@ -26,12 +26,23 @@ export function reducer (
         }
 
         case fromPizzas.LOAD_PIZZAS_SUCCESS: {
-            const data = action.payload;
+            const pizzas = action.payload;
+            const entities = pizzas.reduce(
+                (entities: { [id: number]: Pizza}, pizza: Pizza) => {
+                    return {
+                        ...entities,
+                        [pizza.id]: pizza,
+                    };
+                }, 
+                {
+                    ...state.entities
+                }
+            );
             return {
                 ...state,
                 loading: false,
                 loaded: true,
-                data
+                entities,
             }
         }
 
@@ -50,4 +61,4 @@ export function reducer (
 
 export const getPizzasLoading = (state: PizzaState) => state.loading;
 export const getPizzasLoaded = (state: PizzaState) => state.loaded;
-export const getPizzas = (state: PizzaState) => state.data;
+export const getPizzasEntities = (state: PizzaState) => state.entities;
